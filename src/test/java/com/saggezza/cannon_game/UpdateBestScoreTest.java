bba1211e6e43da83aa85c7ca3354820868406c91
ramshaessa <ressa@luc.edu>
2021-10-06 15:18:03 -0500

    New Changes

--- src/test/java/com/saggezza/cannon_game/UpdateBestScoreTest.java
+++ src/test/java/com/saggezza/cannon_game/UpdateBestScoreTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 public class UpdateBestScoreTest {
 
     @Test
-    public void bestScore() {
+    public void bestScoreReplaced() {
         //Given: I have the best score of 4.
         // And: I have new score of 2.
         int bestScore = 4;
@@ -21,6 +21,26 @@ public class UpdateBestScoreTest {
         //Then: my best score will be 2.
         assertEquals(2, result);
     }
+
+    @Test
+    public void bestScoreNotReplaced() {
+
+        //Given: I have the best score of 4.
+        // And: I have new score of 6.
+
+        int bestScore = 4;
+        int newScore = 6;
+
+        IUpdateBestScore scoreUpdate = new UpdateBestScore();
+        //When: I update my best score.
+        int result = scoreUpdate.computeBestScore(bestScore, newScore);
+
+        //Then: my best score will be 2.
+        assertEquals(6, result);
+    }
+
+
+
 
 
 }
