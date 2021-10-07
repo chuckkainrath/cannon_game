b0c899b25ad8ad078a89acb352ec06bdf1dd8041
ramshaessa <ressa@luc.edu>
2021-10-07 13:14:38 -0500

    More changessss

--- src/main/java/com/saggezza/cannon_game/UpdateFlow.java
+++ src/main/java/com/saggezza/cannon_game/UpdateFlow.java
@@ -4,10 +4,15 @@ public class UpdateFlow implements IUpdateFlow {
 
     private final IPlayerData playerData;
     private final IUpdateBestScore updateBest;
+    private final IUpdateWorstScore updateWorst;
+    private final IUpdateAverageScore updateAverage;
 
-    public UpdateFlow(IPlayerData playerData, IUpdateBestScore updateBest) {
+    public UpdateFlow(IPlayerData playerData, IUpdateBestScore updateBest, IUpdateWorstScore updateWorst, IUpdateAverageScore updateAverage) {
         this.playerData = playerData;
         this.updateBest = updateBest;
+        this.updateWorst = updateWorst;
+        this.updateAverage = updateAverage;
+
     }
 
 
@@ -17,5 +22,9 @@ public class UpdateFlow implements IUpdateFlow {
         double averageScore = playerData.getPlayerAverageScore(name);
         int gamesPlayed = playerData.getPlayerGamesPlayed(name);
         int newBestScore = updateBest.computeBestScore(bestScore, score);
+        int newWorstScore = updateWorst.computeWorstScore(worstScore, score);
+        double newAverageScore = updateAverage.computeAverageScore(averageScore, gamesPlayed, score);
+        int newGamesPlayed = gamesPlayed + 1;
+        playerData.updatePlayer(name, newBestScore, newWorstScore, newGamesPlayed, newAverageScore);
     }
 }
--- src/test/java/com/saggezza/cannon_game/UpdateFlowTest.java
+++ src/test/java/com/saggezza/cannon_game/UpdateFlowTest.java
@@ -20,8 +20,14 @@ public class UpdateFlowTest {
         IUpdateBestScore updateBest = mock(IUpdateBestScore.class);
         when(updateBest.computeBestScore(4,5)).thenReturn(4);
 
+        IUpdateWorstScore updateWorst = mock(IUpdateWorstScore.class);
+        when(updateWorst.computeWorstScore(8,5)).thenReturn(8);
+
+        IUpdateAverageScore updateAverage = mock(IUpdateAverageScore.class);
+        when(updateAverage.computeAverageScore(6.2,12, 5)).thenReturn(6.11);
+
         //When: I update Mansoor's stats.
-        IUpdateFlow updateFlow = new UpdateFlow(playerData, updateBest);
+        IUpdateFlow updateFlow = new UpdateFlow(playerData, updateBest, updateWorst, updateAverage);
         updateFlow.updatePlayer(name, score);
 
         //Then: each of the getBestScore method is called once.
@@ -42,8 +48,14 @@ public class UpdateFlowTest {
         IUpdateBestScore updateBest = mock(IUpdateBestScore.class);
         when(updateBest.computeBestScore(4,5)).thenReturn(4);
 
+        IUpdateWorstScore updateWorst = mock(IUpdateWorstScore.class);
+        when(updateWorst.computeWorstScore(8,5)).thenReturn(8);
+
+        IUpdateAverageScore updateAverage = mock(IUpdateAverageScore.class);
+        when(updateAverage.computeAverageScore(6.2,12, 5)).thenReturn(6.11);
+
         //When: I update Mansoor's stats.
-        IUpdateFlow updateFlow = new UpdateFlow(playerData, updateBest);
+        IUpdateFlow updateFlow = new UpdateFlow(playerData, updateBest, updateWorst, updateAverage);
         updateFlow.updatePlayer(name, score);
 
         //Then: each of the getWorstScore method is called once.
@@ -64,8 +76,14 @@ public class UpdateFlowTest {
         IUpdateBestScore updateBest = mock(IUpdateBestScore.class);
         when(updateBest.computeBestScore(4,5)).thenReturn(4);
 
+        IUpdateWorstScore updateWorst = mock(IUpdateWorstScore.class);
+        when(updateWorst.computeWorstScore(8,5)).thenReturn(8);
+
+        IUpdateAverageScore updateAverage = mock(IUpdateAverageScore.class);
+        when(updateAverage.computeAverageScore(6.2,12, 5)).thenReturn(6.11);
+
         //When: I update Mansoor's stats.
-        IUpdateFlow updateFlow = new UpdateFlow(playerData, updateBest);
+        IUpdateFlow updateFlow = new UpdateFlow(playerData, updateBest, updateWorst, updateAverage);
         updateFlow.updatePlayer(name, score);
 
         //Then: each of the getPlayerAverageScore method is called once.
@@ -88,8 +106,14 @@ public class UpdateFlowTest {
         IUpdateBestScore updateBest = mock(IUpdateBestScore.class);
         when(updateBest.computeBestScore(4,5)).thenReturn(4);
 
+        IUpdateWorstScore updateWorst = mock(IUpdateWorstScore.class);
+        when(updateWorst.computeWorstScore(8,5)).thenReturn(8);
+
+        IUpdateAverageScore updateAverage = mock(IUpdateAverageScore.class);
+        when(updateAverage.computeAverageScore(6.2,12, 5)).thenReturn(6.11);
+
         //When: I update Mansoor's stats.
-        IUpdateFlow updateFlow = new UpdateFlow(playerData, updateBest);
+        IUpdateFlow updateFlow = new UpdateFlow(playerData, updateBest, updateWorst, updateAverage);
         updateFlow.updatePlayer(name, score);
 
 
@@ -111,14 +135,107 @@ public class UpdateFlowTest {
        IUpdateBestScore updateBest = mock(IUpdateBestScore.class);
        when(updateBest.computeBestScore(4,5)).thenReturn(4);
 
+        IUpdateWorstScore updateWorst = mock(IUpdateWorstScore.class);
+        when(updateWorst.computeWorstScore(8,5)).thenReturn(8);
+
+        IUpdateAverageScore updateAverage = mock(IUpdateAverageScore.class);
+        when(updateAverage.computeAverageScore(6.2,12, 5)).thenReturn(6.11);
+
         //When: I update Mansoor's stats.
-        IUpdateFlow updateFlow = new UpdateFlow(playerData, updateBest);
+        IUpdateFlow updateFlow = new UpdateFlow(playerData, updateBest, updateWorst, updateAverage);
         updateFlow.updatePlayer(name, score);
 
-        //Then: each of the getBestScore method is called once.
+        //Then: computeBestScore method is called once.
         verify(updateBest, times(1)).computeBestScore(4,5);
+}
+
+    @Test
+    public void updateWorstScoreOnce() {
+        //Given: Mansoor exists and I score 5.
+        String name = "Mansoor";
+        int score = 5;
+        IPlayerData playerData = mock(IPlayerData.class);
+        when(playerData.getPlayerBestScore(name)).thenReturn(4);
+        when(playerData.getPlayerWorstScore(name)).thenReturn(8);
+        when(playerData.getPlayerAverageScore(name)).thenReturn(6.2);
+        when(playerData.getPlayerGamesPlayed(name)).thenReturn(12);
+
+        IUpdateBestScore updateBest = mock(IUpdateBestScore.class);
+        when(updateBest.computeBestScore(4,5)).thenReturn(4);
+
+        IUpdateWorstScore updateWorst = mock(IUpdateWorstScore.class);
+        when(updateWorst.computeWorstScore(8,5)).thenReturn(8);
+
+        IUpdateAverageScore updateAverage = mock(IUpdateAverageScore.class);
+        when(updateAverage.computeAverageScore(6.2,12, 5)).thenReturn(6.11);
+
+        //When: I update Mansoor's stats.
+        IUpdateFlow updateFlow = new UpdateFlow(playerData, updateBest, updateWorst, updateAverage);
+        updateFlow.updatePlayer(name, score);
+
+        //Then: computeWorstScore method is called once.
+        verify(updateWorst, times(1)).computeWorstScore(8,5);
 
 
+}
+
+    @Test
+    public void updateAverageScoreOnce() {
+        //Given: Mansoor exists and I score 5.
+        String name = "Mansoor";
+        int score = 5;
+        IPlayerData playerData = mock(IPlayerData.class);
+        when(playerData.getPlayerBestScore(name)).thenReturn(4);
+        when(playerData.getPlayerWorstScore(name)).thenReturn(8);
+        when(playerData.getPlayerAverageScore(name)).thenReturn(6.2);
+        when(playerData.getPlayerGamesPlayed(name)).thenReturn(12);
+
+        IUpdateBestScore updateBest = mock(IUpdateBestScore.class);
+        when(updateBest.computeBestScore(4,5)).thenReturn(4);
+
+        IUpdateWorstScore updateWorst = mock(IUpdateWorstScore.class);
+        when(updateWorst.computeWorstScore(8,5)).thenReturn(8);
+
+        IUpdateAverageScore updateAverage = mock(IUpdateAverageScore.class);
+        when(updateAverage.computeAverageScore(6.2,12, 5)).thenReturn(6.11);
+
+        //When: I update Mansoor's stats.
+        IUpdateFlow updateFlow = new UpdateFlow(playerData, updateBest, updateWorst, updateAverage);
+        updateFlow.updatePlayer(name, score);
+
+        //Then: updateAverage method is called once.
+        verify(updateAverage, times(1)).computeAverageScore(6.2,12,5);
 
 
-}}
+    }
+
+    @Test
+    public void writeUpdateOnce() {
+        //Given: Mansoor exists and I score 5.
+        String name = "Mansoor";
+        int score = 5;
+        IPlayerData playerData = mock(IPlayerData.class);
+        when(playerData.getPlayerBestScore(name)).thenReturn(4);
+        when(playerData.getPlayerWorstScore(name)).thenReturn(8);
+        when(playerData.getPlayerAverageScore(name)).thenReturn(6.2);
+        when(playerData.getPlayerGamesPlayed(name)).thenReturn(12);
+
+        IUpdateBestScore updateBest = mock(IUpdateBestScore.class);
+        when(updateBest.computeBestScore(4,5)).thenReturn(4);
+
+        IUpdateWorstScore updateWorst = mock(IUpdateWorstScore.class);
+        when(updateWorst.computeWorstScore(8,5)).thenReturn(8);
+
+        IUpdateAverageScore updateAverage = mock(IUpdateAverageScore.class);
+        when(updateAverage.computeAverageScore(6.2,12, 5)).thenReturn(6.11);
+
+        //When: I update Mansoor's stats.
+        IUpdateFlow updateFlow = new UpdateFlow(playerData, updateBest, updateWorst, updateAverage);
+        updateFlow.updatePlayer(name, score);
+
+        //Then: updatePlayer method is called once.
+        verify(playerData, times(1)).updatePlayer(name, 4, 8, 13, 6.11);
+
+
+    }
+}
