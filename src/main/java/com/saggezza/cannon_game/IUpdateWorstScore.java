de0e9f014214b4b7afa199ae98436991ab8b81e5
ramshaessa <ressa@luc.edu>
2021-10-07 13:16:44 -0500

    More changess

---
+++ src/main/java/com/saggezza/cannon_game/IUpdateWorstScore.java
@@ -0,0 +1,5 @@
+package com.saggezza.cannon_game;
+
+public interface IUpdateWorstScore {
+    int computeWorstScore(int worstScore, int newScore);
+}
---
+++ src/test/java/com/saggezza/cannon_game/UpdateWorstScoreTest.java
@@ -0,0 +1,39 @@
+package com.saggezza.cannon_game;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UpdateWorstScoreTest {
+
+    @Test
+    public void updateWorstScoreWhenNecessary() {
+    //Given: I have the worst score of 4.
+    // And: I scored 2.
+        int worstScore = 2;
+        int newScore = 4;
+
+    // When: I update my worst score.
+
+        IUpdateWorstScore updateWorstScore = new UpdateWorstScore();
+        int newWorstScore = updateWorstScore.computeWorstScore(worstScore, newScore);
+        // Then: my worst score will be 4.
+        assertEquals(4, newWorstScore);
+        }
+
+        @Test
+        public void updateWorstScoreWhenNotNecessary() {
+        // Given: I have the worst score of 4.
+        // And: I scored 2.
+            int worstScore = 4;
+            int newScore = 2;
+
+        // When: I update my worst score.
+
+            IUpdateWorstScore updateWorstScore = new UpdateWorstScore();
+            int newWorstScore = updateWorstScore.computeWorstScore(worstScore, newScore);
+        // Then: my worst score will be 4.
+            assertEquals(4, newWorstScore);
+        }
+    }
+
